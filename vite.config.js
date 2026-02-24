0db943c404e563ede5b65452a88924a6aa218f0e
Samawat Arsalan Hyder <shareeflancer2000@gmail.com>
2026-02-24 23:05:24 +0500

    feat:
    I've implemented code-splitting in your vite.config.js to clear out those large chunk size warnings!
    
    I configured rollupOptions.output.manualChunks to split the massive app.js bundle into smaller, parallel-loading chunks based on your heaviest vendor libraries.

--- vite.config.js
+++ vite.config.js
@@ -12,6 +12,20 @@ export default defineConfig({
         react(),
         tailwindcss(),
     ],
+    build: {
+        chunkSizeWarningLimit: 1000,
+        rollupOptions: {
+            output: {
+                manualChunks: {
+                    react: ['react', 'react-dom'],
+                    inertia: ['@inertiajs/react', '@inertiajs/core'],
+                    mantine: ['@mantine/core', '@mantine/hooks'],
+                    icons: ['@tabler/icons-react'],
+                    vendor: ['axios', 'react-easy-crop'],
+                },
+            },
+        },
+    },
     server: {
         watch: {
             ignored: ['**/storage/framework/views/**'],
